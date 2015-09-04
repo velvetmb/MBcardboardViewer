7e6f3b887f8691efd395889d242aced7edfaa69b
Bảo Hoàng <minhbao92@gmail.com>
2015-09-04 08:21:24 +0100

    First publication
    cardboard viewer v1.1

---
+++ Assets/Main/changeSkyBox.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeSkyBox : MonoBehaviour {
+
+	// Use this for change frames
+	public void changeFrame (Material input) {
+		RenderSettings.skybox = input;
+	}
+}
