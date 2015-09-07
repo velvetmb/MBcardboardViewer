6f14a3caa5d925bbd1b4a9114e99da1eaaacc0ae
Bảo Hoàng <minhbao92@gmail.com>
2015-09-07 08:53:20 +0100

    cardboard viewer v1.2
    - adding icon
    - adding disabled button
    - adding change text function
    - adding welcome message
    - changing Phong Nha sphere image
    - changing default sphere to Khai Dinh Tomb

--- Assets/Main/changeSkyBox.cs
+++ Assets/Main/changeSkyBox.cs
@@ -1,10 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class changeSkyBox : MonoBehaviour {
 
 	// Use this for change frames
 	public void changeFrame (Material input) {
 		RenderSettings.skybox = input;
+	}
+
+	//Use this for change SimpleText
+	public void ChangeText (string input) {
+		GameObject textObject = GameObject.Find("SimpleText");
+		textObject.GetComponent<UnityEngine.UI.Text>().text = input;
+	}
+
+	public void disabledButton (Button input) {
+		GameObject[] listButton = GameObject.FindGameObjectsWithTag ("equirectangular");
+		foreach (GameObject button in listButton) {
+			button.GetComponent<UnityEngine.UI.Button>().interactable = true;
+		}
+		input.interactable = false;
 	}
 }
